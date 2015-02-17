2a42955d3ca5e82c5fbf06e943c85c1ecc8dda72
daryasabinina <dashka.sab@gmail.com>
2015-02-17 21:56:11 +0300

    Yo generate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name busstopApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the busstopApp
+ */
+angular.module('busstopApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
