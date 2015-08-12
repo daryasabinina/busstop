73b519ab6504482f6bd7d05fde2c9fbd46be8341
Darya_Sabinina <darya_sabinina@epam.com>
2015-08-12 17:40:15 +0300

    add favourites to localStorage

--- app/scripts/controllers/busesCtrl.js
+++ app/scripts/controllers/busesCtrl.js
@@ -4,11 +4,27 @@ angular.module('busstopApp')
   .controller('busesCtrl', [ '$scope', 'busesService', 'timeService',
         function ($scope, busesService, timeService) {
 
+        function setFavourites() {
+            $scope.busstopsArray.forEach(function(item) {
+                if (localStorage.getItem(item.stationName) === 'true') {
+                    item.favourite = true;
+                } else {
+                    item.favourite = false;
+                }
+            });
+        }
+
         $scope.searchText = '';
         $scope.busstopsArray = [];
         $scope.timeService = timeService;
 
         busesService.getData().then(function(data){
             $scope.busstopsArray = data;
+            setFavourites();
         });
+
+        $scope.addToFavourites = function(stop) {
+            stop.favourite = !stop.favourite;
+            localStorage.setItem(stop.stationName, stop.favourite);
+        };
   }]);
